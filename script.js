3ea0ef84d7dfb6628b795487481079edad47ea7e
diegoarielsimonelli <simonellidiego@gmail.com>
2021-06-15 15:03:52 -0300

    1

---
+++ script.js
@@ -0,0 +1,40 @@
+// calculo iva
+
+// precioCosto=parseInt(prompt("Ingrese el costo del producto"));
+// console.log ("el precio ingresado es:", precioCosto);
+
+// let iva= 0
+// function iva1(a){
+//     iva= a * 0.21;
+// }
+// iva1(precioCosto);
+// console.log ("el precio con iva  es:", iva);
+
+// let precioTotal=0
+// function sumar(a,b){
+//     precioTotal= a + b
+// }
+// sumar(precioCosto,iva);
+// console.log ("el precio total es:", precioTotal);
+
+// comprobando si es múltiplo o no
+
+// num=parseInt(prompt("Ingrese el n° a verificar"));
+// console.log ("el n° ingresado es:", num);
+// num1=parseInt(prompt("Ingrese el OTRO n° a verificar"));
+// console.log ("el segundo n° a comparar es:", num1);
+
+// let division= 0
+// function div(a,b){
+//     division=a % b
+    
+// }
+
+// div(num,num1);
+// if (division== 0){
+//     console.log (num,"es múltiplo de", num1);
+// } else{
+//     console.log (num,"NO es múltiplo de", num1);
+// }
+
+
