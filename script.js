c2c3e218eae958b010d2931d36a41e9eda37fd1f
diegoarielsimonelli <simonellidiego@gmail.com>
2021-06-18 19:59:38 -0300

    2

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-// calculo iva
+// Calculo iva
 
-// precioCosto=parseInt(prompt("Ingrese el costo del producto"));
-// console.log ("el precio ingresado es:", precioCosto);
+precioCosto=parseInt(prompt("Ingrese el costo del producto"));
+console.log ("el precio ingresado es:"+ precioCosto);
 
-// let iva= 0
-// function iva1(a){
-//     iva= a * 0.21;
-// }
-// iva1(precioCosto);
-// console.log ("el precio con iva  es:", iva);
 
-// let precioTotal=0
-// function sumar(a,b){
-//     precioTotal= a + b
-// }
-// sumar(precioCosto,iva);
-// console.log ("el precio total es:", precioTotal);
+function iva1(a){
+   let iva= a * 0.21;
+   return iva;
+}
+let ivafinal= iva1(precioCosto);
+console.log ("el precio con IVA es:"+ ivafinal);
 
-// comprobando si es múltiplo o no
 
-// num=parseInt(prompt("Ingrese el n° a verificar"));
-// console.log ("el n° ingresado es:", num);
-// num1=parseInt(prompt("Ingrese el OTRO n° a verificar"));
-// console.log ("el segundo n° a comparar es:", num1);
+function sumar(a,b){
+    let precioTotal= a + b;
+    return precioTotal;
+}
+let sumatotal=sumar(precioCosto, ivafinal);
+console.log ("el precio total es $:"+ sumar(precioCosto, sumatotal));
 
-// let division= 0
-// function div(a,b){
-//     division=a % b
-    
-// }
+// // Comprobando si es múltiplo o no
 
-// div(num,num1);
-// if (division== 0){
-//     console.log (num,"es múltiplo de", num1);
-// } else{
-//     console.log (num,"NO es múltiplo de", num1);
-// }
+num=parseInt(prompt("Ingrese el n° a verificar"));
+console.log ("el n° ingresado es:"+ num);
+num1=parseInt(prompt("Ingrese el OTRO n° a verificar"));
+console.log ("el segundo n° a comparar es:"+ num1);
+
+
+function div(a,b){
+    let division=a % b
+    return division;
+}
+let divisionf= div(num,num1);
+
+if (divisionf == 0){
+    console.log (num+" es múltiplo de  " + num1);
+} else{
+    console.log (num+" NO es múltiplo de  "+ num1);
+}
 
 
